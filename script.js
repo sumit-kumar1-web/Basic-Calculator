1f1e19950e73839f7046940b403aa7a951cb8c4f
sumit-kumar1-web <itskrsumit@gmail.com>
2025-07-19 23:58:04 +0530

    complete

---
+++ script.js
@@ -0,0 +1,21 @@
+const input = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+let str = "";
+let arr = Array.from(buttons);
+arr.forEach((buttons) => {
+  buttons.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      str = eval(str);
+      input.value = str;
+    } else if (e.target.innerHTML == "AC") {
+      str = "";
+      input.value = str;
+    } else if (e.target.innerHTML == "Del") {
+      str = str.substring(0, str.length - 1);
+      input.value = str;
+    } else {
+      str += e.target.innerHTML;
+      input.value = str;
+    }
+  });
+});
